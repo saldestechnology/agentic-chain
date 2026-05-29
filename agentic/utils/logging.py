78273592437a542da5f25927c09f7ffd996b7ea3
johanlarar <johan@itkonsult.net>
2026-05-29 22:20:46 +0200

    Added support for none string values
    - Adheres to strict type checks

--- agentic/utils/logging.py
+++ agentic/utils/logging.py
@@ -1,24 +1,36 @@
-from agentic.utils.config import Settings
+from agentic.runnable.runnable import Runnable
+from agentic.utils.config import LogLevel, Settings
 import sys
 import json
 
 settings = Settings()
 
-def log(message: str, log_level: str = "INFO", as_json: bool = False) -> None:
+
+def log(*message: object, log_level: LogLevel = "INFO") -> None:
     if not settings.debug:
         return
-    # Check if instance of Runnable or a function and print name or function name
+
     caller_frame = sys._getframe(1)
     caller_name = caller_frame.f_code.co_name
     caller_locals = caller_frame.f_locals
-    
+
     if "self" in caller_locals:
         instance = caller_locals["self"]
         if hasattr(instance, "name") and instance.name:
             caller_name = instance.name
-        elif type(instance).__name__ == "Runnable" or hasattr(instance, "invoke"):
+        elif isinstance(instance, Runnable):
             caller_name = getattr(instance, "name", type(instance).__name__)
-    if settings.env == 'production':
-        print(json.dumps({ "log_level": log_level, "called_name": caller_name, "message": message }))
-    if settings.env != 'production' and settings.log_level == log_level:
-        print(f"({log_level}) [{caller_name}] {message}")
+
+    if settings.env == "production":
+        print(
+            json.dumps(
+                {
+                    "log_level": log_level,
+                    "called_name": caller_name,
+                    "message": message,
+                },
+                default=str,
+            )
+        )
+    if settings.env != "production" and settings.log_level == log_level:
+        print(f"({log_level}) [{caller_name}] {str(message)}")
